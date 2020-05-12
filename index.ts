9b7106b7f8b0a2db67f949f6c177ebde81cc0ac1
tmurray <tmurray@wumpus-cave.net>
2020-05-12 16:38:58 -0500

    Initial commit

---
+++ index.ts
@@ -0,0 +1,61 @@
+import { v4 as Uuid } from 'uuid';
+import * as FS from 'fs';
+
+const FSPromises = FS.promises;
+
+
+export type storeFunc = (
+    path: string
+) => Promise<string>;
+
+
+export default function (
+    store_dir: string
+): storeFunc
+{
+    return (
+        path: string
+    ): Promise<string> => {
+        const id = Uuid();
+        const digits = id.match( /^([A-Fa-f0-9]{2})([A-Fa-f0-9]{2})/ );
+
+        return new Promise( async (resolve, reject) => {
+            if( 0 < digits.length ) {
+                const first = digits[1];
+                const second = digits[2];
+
+                const dst_dir = [
+                    store_dir
+                    ,first
+                    ,second
+                ].join( "/" );
+
+                // You shouldn't use exceptions as flow control. Unfortunately,
+                // FS.promises didn't get that message.
+                try {
+                    // If this passes OK, then the directory was already there
+                    await FSPromises.access( dst_dir, FS.constants.F_OK );
+                }
+                catch( err ) {
+                    // If we get here, the directory didn't exist, so create it
+                    await FSPromises.mkdir( dst_dir, {
+                        recursive: true
+                    });
+                }
+
+                try {
+                    const dst_path = dst_dir + `/${id}`;
+                    await FSPromises.copyFile( path, dst_path );
+                    await FSPromises.unlink( path );
+                    resolve( dst_path );
+                }
+                catch( err ) {
+                    reject( err );
+                }
+            }
+            else {
+                reject( "Generated UUID did not match regex" );
+            }
+        });
+    };
+}
---
+++ test/stash.ts
@@ -0,0 +1,46 @@
+import FileStore from '../index';
+import * as FS from 'fs';
+import * as GreyGoo from '../index';
+import * as Tap from 'tap';
+
+const STORE_DIR = 'test_dir/store/';
+const TMP_DIR = 'test_dir/tmp/';
+const FSPromises = FS.promises;
+
+Tap.plan( 1 );
+
+
+Tap.test( 'File Store', ( test ) => {
+    test.plan( 2 );
+
+    const store = FileStore( STORE_DIR );
+    const start_file_path = TMP_DIR + "/foo.txt";
+
+    const expected_path_match = new RegExp([
+        STORE_DIR
+        ,"/([A-Fa-f0-9]{2})"
+        ,"/([A-Fa-f0-9]{2})"
+        ,"/\\1\\2[A-Fa-f0-9\\-]+"
+    ].join( "" ) );
+
+    test.comment( `Writing to ${start_file_path}` );
+    FSPromises.writeFile( start_file_path, "bar baz" ).then( () => {
+        return store( start_file_path );
+    }).then( (file_path) => {
+        test.comment( `Stored file at ${file_path}` );
+        FSPromises.access( file_path, FS.constants.F_OK ).then( async () => {
+            test.pass( "File was stored" );
+            // Cleanup
+            await FSPromises.unlink( file_path );
+        }).catch( () => test.fail( "File was not stored" ) );
+    }).then( () => {
+        FSPromises.access( start_file_path, FS.constants.F_OK )
+            .then( async () => {
+                test.fail( "Old file was not removed" );
+                // Cleanup the file that wasn't supposed to be there
+                await FSPromises.unlink( start_file_path );
+            }).catch( () => test.pass( "Old file was removed" ) );
+    }).catch( ( err ) => {
+        test.fail( "Error: " + err );
+    });
+});
